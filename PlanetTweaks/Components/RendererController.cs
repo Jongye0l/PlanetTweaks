9630afb04361dfef4f378e3406919a76249f7acd
Jongyeol <bcpjy1233@gmail.com>
2025-04-28 19:15:59 +0900

    fix: Controller is not Setuped

--- PlanetTweaks/Components/RendererController.cs
+++ PlanetTweaks/Components/RendererController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using JALib.Tools;
+using UnityEngine;
 
 namespace PlanetTweaks.Components;
 
@@ -7,18 +8,17 @@ public class RendererController : MonoBehaviour {
     public static RendererController blueController;
     public static RendererController thirdController;
     public scrPlanet planet;
+    public PlanetRenderer planetRenderer;
     public SpriteRenderer renderer;
 
     private void Awake() {
-        planet = GetComponentInParent<scrPlanet>();
-        renderer = gameObject.AddComponent<SpriteRenderer>();
-        renderer.sortingOrder = planet.planetRenderer.GetComponent<SpriteRenderer>().sortingOrder + 1;
-        renderer.sortingLayerID = planet.planetRenderer.faceDetails.sortingLayerID;
-        renderer.sortingLayerName = planet.planetRenderer.faceDetails.sortingLayerName;
-        scrController controller = scrController.instance;
-        if(planet == controller.planetRed) redController = this;
-        else if(planet == controller.planetBlue) blueController = this;
-        else if(planet == controller.planetGreen) thirdController = this;
+        scrPlanet planet = this.planet = GetComponentInParent<scrPlanet>();
+        SpriteRenderer renderer = this.renderer = gameObject.AddComponent<SpriteRenderer>();
+        PlanetRenderer planetRenderer = this.planetRenderer = planet.planetRenderer;
+        renderer.sortingOrder = planetRenderer.GetComponent<SpriteRenderer>().sortingOrder + 1;
+        renderer.sortingLayerID = planetRenderer.faceDetails.sortingLayerID;
+        renderer.sortingLayerName = planetRenderer.faceDetails.sortingLayerName;
+        SetupStatic();
     }
 
     private void Update() {
@@ -27,6 +27,17 @@ public class RendererController : MonoBehaviour {
             Destroy(gameObject);
             return;
         }
-        if(renderer.enabled != planet.planetRenderer.sprite.visible) renderer.enabled = planet.planetRenderer.sprite.visible;
+        if(renderer.enabled != planetRenderer.sprite.visible) renderer.enabled = planetRenderer.sprite.visible;
+    }
+
+    private void SetupStatic() {
+        scrController controller = scrController.instance;
+        if(!controller) {
+            MainThread.Run(Main.instance, SetupStatic);
+            return;
+        }
+        if(planet == controller.planetRed) redController = this;
+        else if(planet == controller.planetBlue) blueController = this;
+        else if(planet == controller.planetGreen) thirdController = this;
     }
 }
